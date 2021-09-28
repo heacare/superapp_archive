c09a166108809d3d82886bb5919e92453dbe8adf
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2021-09-28 23:56:33 +0800

    add user_repo.dart

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -11,4 +11,29 @@ class User {
   num get bmi {
     return weight / (height * height);
   }
+
+  static fromJson(Map<String, dynamic> data) {
+    final user = User();
+    user.id = data["id"]! as String;
+    user.name = data["name"]! as String;
+    user.birthday = data["birthday"]! as DateTime;
+    user.height = data["height"]! as num;
+    user.weight = data["weight"]! as num;
+    user.country = data["country"]! as String;
+    user.icon = data["icon"]! as String;
+    user.onboardingResponses = data["onboardingResponses"]! as Map<String, String>;
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'birthday': birthday,
+      'height': height,
+      'weight': weight,
+      'country': country,
+      'icon': icon,
+      'onboardingResponses': onboardingResponses,
+    };
+  }
 }
