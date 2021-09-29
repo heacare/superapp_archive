e4b0807697691f08cb47f67740a511d1b3e1c5b5
qihao <ltzehan13@gmail.com>
2021-09-29 15:51:17 +0800

    Update User model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,28 +1,39 @@
 class User {
-  late String id;
-  late String name;
-  late DateTime birthday;
-  late num height;
-  late num weight;
-  late String country;
-  late String icon;
-  late Map<String, String> onboardingResponses;
+  final String id;
+  final String name;
+  final DateTime birthday;
+  final num height;
+  final num weight;
+  final String country;
+  final String icon;
+  final Map<String, String> onboardingResponses;
 
   num get bmi {
     return weight / (height * height);
   }
 
-  static fromJson(Map<String, dynamic> data) {
-    final user = User();
-    user.id = data["id"]! as String;
-    user.name = data["name"]! as String;
-    user.birthday = data["birthday"]! as DateTime;
-    user.height = data["height"]! as num;
-    user.weight = data["weight"]! as num;
-    user.country = data["country"]! as String;
-    user.icon = data["icon"]! as String;
-    user.onboardingResponses = data["onboardingResponses"]! as Map<String, String>;
-  }
+  User(this.id, this.name, this.birthday, this.height, this.weight, this.country, this.icon, this.onboardingResponses);
+
+  // TODO: Remove once connector is up
+  User.testUser() :
+    id = "id",
+    name = "name",
+    birthday = DateTime.now(),
+    height = 169,
+    weight = 42.0,
+    country = "Alaska",
+    icon = "default_icon_id",
+    onboardingResponses = {};
+
+  User.fromJson(Map<String, dynamic> data) :
+    id = data["id"]! as String,
+    name = data["name"]! as String,
+    birthday = data["birthday"]! as DateTime,
+    height = data["height"]! as num,
+    weight = data["weight"]! as num,
+    country = data["country"]! as String,
+    icon = data["icon"]! as String,
+    onboardingResponses = data["onboardingResponses"]! as Map<String, String>;
 
   Map<String, dynamic> toJson() {
     return {
