4f22bb1788391773af19967afdce67139daf02e9
qihao <ltzehan13@gmail.com>
2021-10-14 16:13:23 +0800

    Styled content.dart

--- lib/screens/content.dart
+++ lib/screens/content.dart
@@ -15,26 +15,70 @@ class ContentScreen extends StatefulWidget {
 class _ContentScreenState extends State<ContentScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-            child:  ListView.builder(
-                itemCount: widget.content.chapters.length,
-                itemBuilder: (context, index) {
-                  final chapter = widget.content.chapters[index];
 
-                  return TextButton(
-                      child: ListTile(
-                        leading: const Icon(Icons.bookmark),
-                        title: Text(chapter.title),
-                      ),
-                      onPressed: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) =>
-                                ChapterScreen(chapter: chapter)));
-                      });
-                },
-            )
+    final chapterListView = ListView.separated(
+      separatorBuilder: (BuildContext context, int index) {
+        return const SizedBox(height: 8.0);
+      },
+      padding: const EdgeInsets.all(16.0),
+      itemCount: widget.content.chapters.length,
+      itemBuilder: (context, index) {
+        final chapter = widget.content.chapters[index];
+
+        return Card(
+          child: InkWell(
+            onTap: () => Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => ChapterScreen(chapter: chapter))
+            ),
+            child: Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(chapter.title, style: Theme.of(context).textTheme.headline2!.copyWith(fontWeight: FontWeight.bold)),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Icon(Icons.access_time_outlined, color: Colors.grey[600], size: 18.0),
+                            const SizedBox(width: 4.0),
+                            // TODO Pull from chapter model
+                            Text("5 mins", style: Theme.of(context).textTheme.bodyText1!.copyWith(height: 1.0, color: Colors.grey[600]))
+                          ]
+                        )
+                      ],
+                    ),
+                    const SizedBox(height: 8.0),
+                    Text(
+                      "Sleep 1XX",
+                      style: TextStyle(color: Theme.of(context).textTheme.headline1!.color, fontWeight: FontWeight.bold)
+                    )
+                  ],
+                )
+            ),
+          ),
+          color: Colors.grey[200],
+        );
+      },
+    );
+
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                child: Text(widget.content.title, style: Theme.of(context).textTheme.headline1),
+                padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0)
+              ),
+              Expanded(
+                child: chapterListView
+              )
+            ]
         )
+      )
     );
   }
 }
--- lib/screens/contents.dart
+++ lib/screens/contents.dart
@@ -18,6 +18,67 @@ class _ContentsScreenState extends State<ContentsScreen> {
   @override
   Widget build(BuildContext context) {
 
+    final contentListView = StreamBuilder<QuerySnapshot<Content>>(
+      stream: contents.getAll(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text("An error occured");
+        }
+
+        if (!snapshot.hasData) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        final data = snapshot.requireData;
+        return Expanded(
+          child: ListView.separated(
+              separatorBuilder: (BuildContext context, int index) {
+                return const SizedBox(height: 8.0);
+              },
+              padding: const EdgeInsets.all(16.0),
+              itemCount: data.size,
+              itemBuilder: (context, index) {
+                final content = data.docs[index].data();
+
+                return Card(
+                  child: InkWell(
+                    onTap: () => Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => ContentScreen(content: content))
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(24.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(content.title, style: Theme.of(context).textTheme.headline2!.copyWith(fontWeight: FontWeight.bold)),
+                              Container(
+                                child: const Padding(
+                                  child: Text("+5 years", style: TextStyle(color: Colors.white, fontSize: 16.0, fontWeight: FontWeight.bold, height: 1.0)),
+                                  padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0)
+                                ),
+                                decoration: BoxDecoration(
+                                  color: Theme.of(context).colorScheme.primary,
+                                  borderRadius: BorderRadius.circular(16.0)
+                                ),
+                              )
+                            ],
+                          ),
+                          const SizedBox(height: 8.0),
+                          // TODO Pull from content model
+                          const Text("Lorem Ipsum")
+                        ],
+                      )
+                    ),
+                  ),
+                  color: Colors.grey[200],
+                );
+              })
+        );
+      });
+
     return Scaffold(
       body: SafeArea(
         child: Column(
@@ -27,67 +88,7 @@ class _ContentsScreenState extends State<ContentsScreen> {
               child: Text("Content", style: Theme.of(context).textTheme.headline1),
               padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0)
             ),
-
-            StreamBuilder<QuerySnapshot<Content>>(
-                stream: contents.getAll(),
-                builder: (context, snapshot) {
-                  print(snapshot);
-                  if (snapshot.hasError) {
-                    return Text("An error occured");
-                  }
-
-                  if (!snapshot.hasData) {
-                    return const Center(child: CircularProgressIndicator());
-                  }
-
-                  final data = snapshot.requireData;
-                  return Expanded(
-                      child: ListView.separated(
-                        separatorBuilder: (BuildContext context, int index) {
-                          return const SizedBox(height: 8.0);
-                        },
-                        padding: const EdgeInsets.all(16.0),
-                        itemCount: data.size,
-                        itemBuilder: (context, index) {
-                          final content = data.docs[index].data();
-
-                          return Card(
-                            child: InkWell(
-                              onTap: () => Navigator.of(context).push(
-                                MaterialPageRoute(builder: (context) => ContentScreen(content: content))
-                              ),
-                              child: Padding(
-                                  padding: const EdgeInsets.all(24.0),
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                                    children: [
-                                      Row(
-                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Text(content.title, style: Theme.of(context).textTheme.headline2!.copyWith(fontWeight: FontWeight.bold)),
-                                          Container(
-                                            child: const Padding(
-                                                child: Text("+5 years", style: TextStyle(color: Colors.white, fontSize: 16.0, fontWeight: FontWeight.bold, height: 1.0)),
-                                                padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0)
-                                            ),
-                                            decoration: BoxDecoration(
-                                                color: Theme.of(context).colorScheme.primary,
-                                                borderRadius: BorderRadius.circular(16.0)
-                                            ),
-                                          )
-                                        ],
-                                      ),
-                                      const SizedBox(height: 8.0),
-                                      const Text("Lorem Ipsum")
-                                    ],
-                                  )
-                              ),
-                            ),
-                            color: Colors.grey[200],
-                          );
-                        })
-                  );
-            })
+            contentListView
           ]
         )
       )
