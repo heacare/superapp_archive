e5cc9f1b71f88651b2b9c05ac10fbfbcd2e6e319
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2021-11-15 14:30:49 +0800

    add user service

--- backend/src/api/api.module.ts
+++ backend/src/api/api.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { DbModule } from 'src/db/db.module';
 import { ContentController } from './content/content.controller';
 import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [DbModule],
   controllers: [ContentController, UserController],
+  providers: [UserService],
 })
 export class ApiModule {}
