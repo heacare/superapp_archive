09202107f0e375882084bdf65e5b7496453adcd7
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2021-11-15 15:44:37 +0800

    add docker-compose for dev

--- backend/src/api/api.module.ts
+++ backend/src/api/api.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { DbModule } from 'src/db/db.module';
 import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
 import { ContentController } from './content/content.controller';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
@@ -8,6 +9,6 @@ import { UserService } from './user/user.service';
 @Module({
   imports: [DbModule],
   controllers: [ContentController, UserController, AuthController],
-  providers: [UserService],
+  providers: [UserService, AuthService],
 })
 export class ApiModule {}
