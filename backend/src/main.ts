9f2e72187a76dc497a27631ec3f9e15ff848a032
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2021-11-28 18:53:00 +0800

    add openapi

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { writeFileSync } from 'fs';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('HEA Backend')
+    .setDescription("API for HEA's backend")
+    .setVersion('0.5')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // writes most recent OpenAPI document to file system
+  writeFileSync('./openapi.json', JSON.stringify(document));
+
   await app.listen(3000);
 }
 bootstrap();
