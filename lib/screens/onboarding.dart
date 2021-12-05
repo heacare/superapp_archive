0aa01f2e82a947230c528f610c8daff03792f30b
Daniel Lim <7555ic@gmail.com>
2021-12-05 07:05:16 +0000

    fix: deleted more obsolete code from onboarding.dart

--- lib/screens/onboarding.dart
+++ lib/screens/onboarding.dart
@@ -1,28 +1,12 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:colorful_safe_area/colorful_safe_area.dart';
-import 'package:date_time_picker/date_time_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_form_builder/flutter_form_builder.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:hea/providers/auth.dart';
-import 'package:hea/screens/error.dart';
-import 'package:hea/widgets/firebase_svg.dart';
-import 'package:hea/widgets/onboard_progress_bar.dart';
-import 'package:health/health.dart';
-import 'package:url_launcher/url_launcher.dart';
-
 import 'package:hea/data/user_repo.dart';
-import 'package:hea/models/onboarding_custom.dart';
-import 'package:hea/models/onboarding_template.dart';
 import 'package:hea/models/user.dart';
-import 'home.dart';
 
+import 'package:hea/screens/home.dart';
 import 'package:hea/screens/onboarding/start.dart';
 import 'package:hea/screens/onboarding/health_setup.dart';
 import 'package:hea/screens/onboarding/basic_info.dart';
-
-const onboardingStartId = "onboard_start";
-const onboardingLastId = "birth_control_2";
 
 enum OnboardingStep {
   health_sync,
@@ -52,9 +36,6 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
 
   // No reflections so we have to update through a map
   late final Map<String, dynamic> userJson;
-
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final GlobalKey<OnboardProgressBarState> _progressBarKey = GlobalKey<OnboardProgressBarState>();
 
   @override
   initState() {
@@ -114,19 +95,5 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   Widget build(BuildContext context) {
     _reroute();
     return Container();
-    // return FutureBuilder<OnboardingTemplateMap>(
-    //   future: templateMapFuture,
-    //   builder: _templateBuilder
-    // );
-  }
-
-  _launchUrl(String url) async {
-    if (await canLaunch(url)) {
-      // Try to open web view within the app
-      await launch(url, forceWebView: true, forceSafariVC: true);
-    }
-    else {
-      throw "Failed to open $url";
-    }
   }
 }
