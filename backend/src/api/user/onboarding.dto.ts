61379bc6c54f5e7d432ec6ee0b585887d768a5b2
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2021-12-17 20:13:33 +0800

    prettier

--- backend/src/api/user/onboarding.dto.ts
+++ backend/src/api/user/onboarding.dto.ts
@@ -1,6 +1,5 @@
 // TODO actually fill these with valid values from the Notion
 
-import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import {
   IsDateString,
--- backend/src/api/user/user.service.ts
+++ backend/src/api/user/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { OnboardingDto, SmokerInfo } from './onboarding.dto';
+import { OnboardingDto } from './onboarding.dto';
 import { User } from './user.entity';
 
 @Injectable()
