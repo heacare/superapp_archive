a7c39931d0173fc158d92ca20312bd9bf614f667
Sudharshan <sudhar393@gmail.com>
2022-05-12 17:11:16 +0800

    chore: upgraded from nuxt 2 to nuxt 3

--- dashboard/app.vue
+++ dashboard/app.vue
@@ -7,10 +7,6 @@
   </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue';
-
-export default Vue.extend({
-  name: 'IndexPage',
-});
+<script setup lang="ts">
 </script>
+
--- dashboard/tailwind.config.js
+++ dashboard/tailwind.config.js
@@ -1,11 +1,7 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 module.exports = {
+  content: [],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
-      },
-    },
+    extend: {},
   },
-};
+  plugins: [],
+}
