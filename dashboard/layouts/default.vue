5b59dced000eb5a9ea820546536c0e7b0b5b2593
Ambrose Chua <ambrose+hea@hey.com>
2022-05-17 19:13:23 +0800

    style(dashboard): Fix lint checker

--- dashboard/layouts/default.vue
+++ dashboard/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="flex flex-row items-center p-6">
-    <img src="logo.png" class="w-8" />
+    <img src="../assets/logo.png" class="w-8" />
     <h1 class="text-gray-600 font-semibold text-md ml-2">Healer Dashboard</h1>
   </nav>
   <main>
